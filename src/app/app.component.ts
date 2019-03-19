4348954964360de87fbfda5d2239da075902511f
greif.matthias <greif.matthias@outlook.com>
2019-03-19 11:28:35 +0100

    IMPR: Sample updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Moment } from 'moment';
+import { CalendarComponent } from 'calendar';
 
 @Component({
   selector: 'lc-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'lib-calendar';
+export class AppComponent implements OnInit {
+
+  @ViewChild('calendar') calendar : CalendarComponent;
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+  // Get onselectionchange event from calendar
+  doChange(event : Moment[]){
+    console.log(event.length);
+  }
+
+  // Trigger today selection of calendar
+  doSelectToday(){
+    this.calendar.doSelectToday();
+  }
 }
